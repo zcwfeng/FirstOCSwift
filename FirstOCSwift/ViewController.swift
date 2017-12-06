89ea44766f7482e8cbc22d9de6efcc91286c1887
zhangchuanwei <zhangchuanwei@fotoable.com>
2017-12-06 22:39:02 +0800

    Initial Commit

---
+++ FirstOCSwift/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  FirstOCSwift
+//
+//  Created by zcw on 2017/12/6.
+//  Copyright © 2017年 zcw. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
